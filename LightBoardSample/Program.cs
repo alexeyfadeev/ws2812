8472b1bff483f81054ec0e278a0bde1898c70ac1
Alexey Fadeev <fadeevas@sibedge.com>
2021-01-21 13:14:16 +0700

    RenderBitmap method added

--- LightBoardSample/Program.cs
+++ LightBoardSample/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
-
-namespace LightBoardSample
+﻿namespace LightBoardSample
 {
+    using System.Drawing;
+    using System.Net;
+    using RedAlliance.LightBoardLib;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var prog = new Program();
+            prog.Run();
+        }
+
+        public void Run()
+        {
+            var manager = new LightBoardManager(64, 8, 128);
+
+            using (var bmp = SaveImage("https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/81/81f0106f666b80042c4b4d1c2ffc50d0f010d155_medium.jpg"))
+            {
+                manager.RenderBitmap(bmp);
+            }
+        }
+
+        private Bitmap SaveImage(string url)
+        {
+            using (var client = new WebClient())
+            using (var stream = client.OpenRead(url))
+            {
+                var bitmap = new Bitmap(stream);
+                return bitmap;
+            }
         }
     }
 }
--- RedAlliance.LightBoardLib/LightBoardManager.cs
+++ RedAlliance.LightBoardLib/LightBoardManager.cs
@@ -1,23 +1,60 @@
 ﻿namespace RedAlliance.LightBoardLib
 {
+    using System.Drawing;
     using rpi_ws281x;
     using Unosquare.RaspberryIO;
     using Unosquare.WiringPi;
 
     public class LightBoardManager
     {
+        private Controller _controller;
+        private WS281x _rpi;
+
         public LightBoardManager()
         {
-            
+            this.Initialize();
+        }
+
+        public LightBoardManager(int screenWidth, int screenHeight, byte brightness)
+        {
+            ScreenWidth = screenWidth;
+            ScreenHeight = screenHeight;
+            Brightness = brightness;
+            this.Initialize();
         }
         
-        public int ScreenWidth { get; private set } = 64;
-        public int ScreenHeight { get; private set } = 8;
-        public int LedCount => ScreenWidth * ScreenHeight;
+        public int ScreenWidth { get; private set; } = 64;
         
+        public int ScreenHeight { get; private set; } = 8;
+        
+        public byte Brightness { get; private set; } = 255;
+        
+        public int LedCount => ScreenWidth * ScreenHeight;
+
+        public void RenderBitmap(Bitmap bmp)
+        {
+            for (int i = 0; i < ScreenWidth; i++)
+            {
+                bool reverseMode = i % 2 == 1;
+
+                for (int j = 0; j < ScreenHeight; j++)
+                {
+                    var pixelY = reverseMode ? (ScreenHeight - j - 1) : j;
+
+                    _controller.SetLED(i * ScreenHeight + j, bmp.GetPixel(i, pixelY));
+                }
+            }
+
+            _rpi.Render();
+        }
+
         private void Initialize()
         {
             Pi.Init<BootstrapWiringPi>();
+            var settings = Settings.CreateDefaultSettings();
+
+            _rpi = new WS281x(settings);
+            _controller = settings.AddController(LedCount, Pin.Gpio18, StripType.WS2812_STRIP, ControllerType.PWM0, Brightness, false);
         }
     }
 }
